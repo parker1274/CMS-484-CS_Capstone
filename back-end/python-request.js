6ab757ee6fcc38a5a7726d067e361ed2ba1db5a8
Jack Krantz <jkrantz0508@gmail.com>
2024-04-08 09:35:22 -0400

    Added new python files and their back-end calls

--- back-end/python-request.js
+++ back-end/python-request.js
@@ -1,8 +1,10 @@
 const { spawn } = require('child_process');
 
+
+// Outcome prediction function
 function predictionRequest(params) {
   return new Promise((resolve, reject) => {
-    const pythonProcess = spawn('python3', ['./main.py', ...params]);
+    const pythonProcess = spawn('python3', ['./python-files/main.py', ...params]);
 
     let outputData = '';
     pythonProcess.stdout.on('data', (data) => {
@@ -30,4 +32,67 @@ function predictionRequest(params) {
   });
 }
 
-module.exports = { predictionRequest };
+// Actionable data function
+function actionableDataRequest(params) {
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn('python3', ['./python-files/actionableData.py', ...params]);
+
+    let outputData = '';
+    pythonProcess.stdout.on('data', (data) => {
+      outputData += data.toString();
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+      reject(new Error(`stderr: ${data}`));  // Reject the promise on error
+    });
+
+    pythonProcess.on('close', (code) => {
+      if (code === 0) {
+        try {
+          const jsonData = JSON.parse(outputData);
+          console.log('Result from Python:', jsonData);
+          resolve(jsonData);  // Resolve the promise with the JSON data
+        } catch (error) {
+          reject(new Error('Failed to parse JSON output'));
+        }
+      } else {
+        reject(new Error(`Python script exited with code ${code}`));
+      }
+    });
+  });
+}
+
+// Team performance insight (tpi) function
+function tpiRequest(params) {
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn('python3', ['./python-files/tpiData.py', ...params]);
+
+    let outputData = '';
+    pythonProcess.stdout.on('data', (data) => {
+      outputData += data.toString();
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+      reject(new Error(`stderr: ${data}`));  // Reject the promise on error
+    });
+
+    pythonProcess.on('close', (code) => {
+      if (code === 0) {
+        try {
+          const jsonData = JSON.parse(outputData);
+          console.log('Result from Python:', jsonData);
+          resolve(jsonData);  // Resolve the promise with the JSON data
+        } catch (error) {
+          reject(new Error('Failed to parse JSON output'));
+        }
+      } else {
+        reject(new Error(`Python script exited with code ${code}`));
+      }
+    });
+  });
+}
+
+
+module.exports = { predictionRequest, actionableDataRequest, tpiRequest };
